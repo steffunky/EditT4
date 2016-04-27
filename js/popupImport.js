1cd1cdb7c0f8ab8a900d141f9787169b0d4e79b7
AntoineThebaud <antoine.thebaud@yahoo.fr>
2016-04-27 09:50:33 +0200

    Import : it starts to work..

--- js/popupImport.js
+++ js/popupImport.js
@@ -26,42 +26,36 @@
       var $close_button,          $create_notions_block,
           $create_notions_button, $open_block, $open_button;
 
+      $open_block = this.createBlock();
+      $open_block.append($open_button);
+      
       // file selector
       $open_file = this.createInputFile();
       $open_file.on({
         change: (function(_this) {
           return function() {
-            return _this.load(this.value);
+            return _this.load(this.value, $open_block);
           };
         })(this)
       });
-
-      $open_block = this.createBlock();
-      $open_block.append($open_button);
       $open_block.append($open_file);
 
-      // Block to create a tree from scratch (directly create notions)
-      $create_notions_button = this.createButton('Create from scratch');
-      $create_notions_button.on({
-        click: (function(_this) {
-          return function() {
-            return _this.$popupimport.trigger('createNotions');
-          };
-        })(this)
-      });
-      $create_notions_block = this.createBlock('Create from scratch');
-      $create_notions_block.append($create_notions_button);
       $close_button = this.createCloseButton();
-      this.$popupimport = this.createPopup([this.createTitle('Import')], [$open_block, $create_notions_block], [$close_button]);
+      this.$popupimport = this.createPopup([this.createTitle('Import')], $open_block, $close_button);
       return this.applyCloseButtonEvents($close_button, this.$popupimport);
     };
 
     // Load data from file
-    PopupImport.prototype.load = function(file) {
+    PopupImport.prototype.load = function(file, $open_block) {
       //TODO : read file 
       console.log(file);
       var fs = require('fs');
-      fs.readFile(file, 'utf8', function (err,data) {
+      var data;
+
+      var $group = this.createFieldset('Cell display');
+
+      //synchronous because we want to use the data below
+      fs.readFileSync(file, 'utf8', function (err,data) {
         if (err) {
           return console.log(err);
         }
@@ -71,63 +65,74 @@
         data = JSON.parse(data);
         console.log('data after :');
         console.log(data);
+      });//TODO : catch error ?
 
-        //TODO : display content
-        //notions :
-        var file_notions = data.notions;
 
-        var notion;
-/*        for (var i = 0, len = file_notions.length; i < len; i++) {
-          notion = file_notions[i];
-          console.log(i + 'e notions :' + notion['name']);
-        }*/
+/*
 
-        var $body = $('<div></div>');
+      //TODO : display content
+      //notions :
+      console.log('data out :');
+      console.log(data);
+      var file_notions = data.notions;
+
+      var notion;
+      for (var i = 0, len = file_notions.length; i < len; i++) {
+        notion = file_notions[i];
+        console.log(i + 'e notions :' + notion['name']);
+      }
+
+      var $body = $('<div></div>');
+
+      var $group = this.createFieldset('Cell display');
+      $group.attr('id', 'cell_display');
       
-        // Cell/tooltip display
-/*        var $group = this.createFieldset('Cell display');
-        $group.attr('id', 'cell_display');*/
-        
-        // Create table
-        for (var i = 0, len = file_notions.length; i < len; i++) {
+      // Cell/tooltip display
+      //TODO : Ca casse tout !
+      //$group.attr('id', 'cell_display');
+      
+      // Create table
 
-          notion = file_notions[i];
-          var $title = $('<caption></caption>').text(notion['name']);
-          var $tr = $('<tr></tr>');
-          $tr.append([$('<th></th>').text('Cell'), $('<th></th>').text('Tooltip')]);
-          var $table = $('<table></table>').addClass('table_notion_display').append([$title, $tr]);
+      //for each notion
+      for (var i = 0, len = file_notions.length; i < len; i++) {
 
-          var instances = notion['class_instances'];
-          var instance;
-          for (var j = 0, lenb = instances.length; j < lenb; j++) {
-            //TODO
-          }
+        notion = file_notions[i];
+        var $title = $('<caption></caption>').text(notion['name']);
+        var $tr = $('<tr></tr>');
+        $tr.append([$('<th></th>').text('Cell'), $('<th></th>').text('Tooltip')]);
+        var $table = $('<table></table>').addClass('table_notion_display').append([$title, $tr]);
 
-/*          ref = notion.getDisplayAttributes();
-          
-          // Create lines
-          for (attribute in ref) {
-            display = ref[attribute];
-            $tr = $('<tr></tr>');
-            ref1 = ['cell', 'tooltip'];
-            
-            // Create cells
-            for (j = 0, len1 = ref1.length; j < len1; j++) {
-              key = ref1[j];
-              $label = $('<label></label>').addClass('label_display').text(attribute);
-              $input = this.createCheckbox('', attribute, display[key]);
-              $label.prepend($input);
-              $td = $('<td></td>').append($label);
-              $tr.append($td);
-            }
-            $table.append($tr);
-          }*/
-          $group.append($table);
+        var instances = notion['class_instances'];
+        var instance;
+        for (var j = 0, lenb = instances.length; j < lenb; j++) {
+          instance = instances[j];
+          console.log(instance);
         }
-        $body.append($group);
 
+        ref = notion.getDisplayAttributes();
+        
+        // Create lines
+        for (attribute in ref) {
+          display = ref[attribute];
+          $tr = $('<tr></tr>');
+          ref1 = ['cell', 'tooltip'];
+          z
+          // Create cells
+          for (j = 0, len1 = ref1.length; j < len1; j++) {
+            key = ref1[j];
+            $label = $('<label></label>').addClass('label_display').text(attribute);
+            $input = this.createCheckbox('', attribute, display[key]);
+            $label.prepend($input);
+            $td = $('<td></td>').append($label);
+            $tr.append($td);
+          }
+          $table.append($tr);
+        }
+        $group.append($table);
+      }
+      $body.append($group);*/
 
-      });
+      return $open_block.append('<p>JE VOUDRAIS JUSTE TESTER ENCORE MERCI</p>');
     };
 
     // Show the popup
