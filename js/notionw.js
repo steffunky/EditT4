60f015f874c73703a89d8f02514818ed73424937
Steffunky <stephen@stephen.ovh>
2017-05-16 11:18:18 +0000

    Correction du bug background popup et ajout bouton notions

--- js/notionw.js
+++ js/notionw.js
@@ -1,8 +1,9 @@
 (function() {
   $(function() {
-    console.log("yo");
     $("body").addClass('dropper');
-
+    while(global.is_ldd != 1){
+      ;
+    }
     var popupNotions = new PopupNotions();
     popupNotions.create();
     popupNotions.show();
@@ -12,27 +13,18 @@
     $(".valid_button").on({
       click: (function(_this) {
         return function() {
-          console.log("hihi");
-          console.log(popupNotions.notions);
-          console.log("allo");
-          global.dic["pn"].$popupnotions.trigger('notionsCreated', [popupNotions.notions]);
-          global.dic["pn"].close();
+
+          global.popupNotions.$popupnotions.trigger('notionsCreated', [popupNotions.notions]);
+          global.popupNotions.close();
           return false;
         };
       })(this)
     });
-  global.dic["add_notion"].click();
-  //global.dic["pn"].$popupnotions.popup('hide');
-  //global.dic["pn"].$popupnotions.empty();
-  //console.log(global.dic["pn"].$popupnotions.find(".close_button"));
-  console.log(global.dic["nf"]);
 
-  global.dic["pn"].close();
-  global.dic["html"].click();
-  //global.dic["cb"].click();
-  //global.dic["pn"].hide();
-  console.log(global.dic["nf"]);
-  //global.dic["facto"](global.dic["nf"]);
-  //global.dic["pn"].close();
+    //popup in original window
+    global.dic["add_notion"].click();
+    setTimeout(function(){
+      global.popupNotions.close();
+    }, 100);
   });
 }).call(this);
